ff1a4cde0408499d3735f97d77331062a4aa889b
Next() <1727728279@qq.com>
2023-07-04 17:31:01 +0800

    完善分类

--- src/main/java/com/example/blogsystem/controller/CategoryController.java
+++ src/main/java/com/example/blogsystem/controller/CategoryController.java
@@ -4,6 +4,7 @@ import com.baomidou.mybatisplus.extension.api.R;
 import com.example.blogsystem.common.Result;
 import com.example.blogsystem.entity.Category;
 import com.example.blogsystem.mapper.CategoryMapper;
+import com.example.blogsystem.util.RedisUtil;
 import org.apache.ibatis.annotations.Param;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.support.SimpleTriggerContext;
@@ -27,15 +28,21 @@ public class CategoryController {
 
     @Autowired
     CategoryMapper categoryMapper;
+    @Autowired
+    RedisUtil redisUtil;
 
     /**
-     * 获取全部分类
+     * 获取全部分类(前五条)
      * @return
      */
     @GetMapping("/getCate")
-    public Result<List> getAllCate(){
+    public Result<HashMap> getAllCate(){
         List<Category> list = categoryMapper.getAllCate();
-        return Result.ok(list);
+        Integer count = categoryMapper.getCateCount("");
+        HashMap<String,Object> map = new HashMap();
+        map.put("list",list);
+        map.put("count",count);
+        return Result.ok(map);
     }
 
     /**
@@ -50,6 +57,8 @@ public class CategoryController {
         if(res.size() == 0){
             Integer count = categoryMapper.addCate(category.getCategoryName(),category.getCategoryAvatar(),category.getCategoryDetail());
             if(count > 0){
+                //新增数据后清空redis缓存
+                redisUtil.cleanAll();
                 return Result.ok("添加成功");
             }else {
                 return Result.fail("添加失败,请重试");
@@ -73,6 +82,8 @@ public class CategoryController {
         }else {
             Integer count = categoryMapper.updateCate(category);
             if(count > 0){
+                //更新数据后清空redis缓存
+                redisUtil.cleanAll();
                 return Result.ok("修改成功");
             }else {
                 return Result.fail("修改失败");
@@ -87,9 +98,15 @@ public class CategoryController {
      */
     @PostMapping("/delCate")
     public Result<String> delCate(@RequestParam("id") Integer id){
-        //TODO:删除前判断在tb_blog表中是否有关联的博客
+        //删除前判断在tb_blog表中是否有关联的博客
+        Integer rel = categoryMapper.getCateBlog(id);
+        if(rel > 0){
+            return Result.fail("该分类存在关联的博客，删除失败");
+        }
         Integer count = categoryMapper.delCate(id);
         if(count > 0){
+            //删除数据后清空redis缓存
+            redisUtil.cleanAll();
             return Result.ok("删除成功");
         }else {
             return Result.fail("删除失败");
@@ -103,22 +120,62 @@ public class CategoryController {
      * @return
      */
     @PostMapping("/pagin")
-    public Result<List> pagin(@RequestParam("pageNum") Integer pageNum,@RequestParam("pageSize") Integer pageSize){
-        //将pageNum进行转换：pageNum：第几页，但是数据库limit需要的是开始查询的位置
-        Integer start  = (pageNum - 1) * pageSize;//开始查询的位置
-        List<Category> cateList = categoryMapper.paginUsers(start,pageSize);
+    public Result<HashMap> pagin(@RequestParam("categoryName") String categoryName,@RequestParam("pageNum") Integer pageNum
+                                ,@RequestParam("pageSize") Integer pageSize){
+        //从redis获取数据
+        List<Category> cacheCate =(List<Category>) redisUtil.getCache("tb_category:"+pageNum+"-"+pageSize+":"+"categoryName"+":"+categoryName);
+        Integer cacheCount = (Integer) redisUtil.getCache("tb_category:count:"+pageNum+"-"+pageSize+":"+"categoryName"+":"+categoryName);
+        //redis中没有缓存，再重新获取
+        if(cacheCate==null || cacheCount==null){
+            //拿到数据总数
+            cacheCount = categoryMapper.getCateCount(categoryName);
+            //将pageNum进行转换：pageNum：第几页，但是数据库limit需要的是开始查询的位置
+            Integer start  = (pageNum - 1) * pageSize;//开始查询的位置
+            cacheCate = categoryMapper.paginUsers(categoryName,start,pageSize);
+            //将查询的数据放入Redis中
+            redisUtil.setCache("tb_category:"+pageNum+"-"+pageSize+":"+"categoryName"+":"+categoryName,cacheCate);
+            redisUtil.setCache("tb_category:count:"+pageNum+"-"+pageSize+":"+"categoryName"+":"+categoryName,cacheCount);
+            System.out.println("分页数据：加入缓存");
+        }else{
+            System.out.println("分页数据：使用缓存");
+        }
         //可以使用HashMap来保存总条数以及查询结果一起发送给前端
-        return Result.ok(cateList);
+        HashMap<String,Object> map = new HashMap();
+        map.put("count",cacheCount);
+        map.put("list",cacheCate);
+        return Result.ok(map);
     }
 
+    /**
+     * 批量删除
+     * @param data
+     * @return
+     */
     @PostMapping("/delBatch")
     public Result<String> delBatch(@RequestBody Map<String,List<Integer>> data) {
         List<Integer> ids = data.get("ids");
+        //删除前判断在tb_blog表中是否有关联的博客
+        for (Integer item: ids) {
+            //删除前判断在tb_blog表中是否有关联的博客
+            Integer rel = categoryMapper.getCateBlog(item);
+            if(rel > 0){
+                return Result.fail("选择的分类存在关联的博客，删除失败");
+            }
+        }
         int count = categoryMapper.delBatch(ids);
         if(count >0){
+            //删除数据后清空redis缓存
+            redisUtil.cleanAll();
             return Result.ok("删除成功");
         }else{
             return Result.fail("删除失败");
         }
+    }
+
+    @GetMapping("/hotCate")
+    public Result<List<HashMap>> getHotCate(){
+        //获取到各个分类绑定的博客数量 并排序 获取前十条
+        List<HashMap> map = categoryMapper.getHotCate();
+        return Result.ok(map);
     }
 }
--- src/main/java/com/example/blogsystem/mapper/CategoryMapper.java
+++ src/main/java/com/example/blogsystem/mapper/CategoryMapper.java
@@ -6,6 +6,7 @@ import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import org.apache.ibatis.annotations.*;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import java.util.HashMap;
 import java.util.List;
 
 /**
@@ -20,7 +21,7 @@ import java.util.List;
 public interface CategoryMapper extends BaseMapper<Category> {
 
     //获取所有分类信息
-    @Select("select * from tb_category")
+    @Select("select * from tb_category limit 0,5")
     List<Category> getAllCate();
 
     //新增分类
@@ -39,9 +40,23 @@ public interface CategoryMapper extends BaseMapper<Category> {
     @Delete("delete from tb_category where id=#{id}")
     Integer delCate(@Param("id") Integer id);
 
-    //分页查询
-    @Select("select  * from tb_category limit #{pageNum},#{pageSize}")
-    List<Category> paginUsers(@Param("pageNum") Integer pageNum,@Param("pageSize") Integer pageSize);
+    //分页查询(带模糊搜索分类名)
+    @Select("select  * from tb_category where category_name like concat('%',#{categoryName},'%') limit #{pageNum},#{pageSize}")
+    List<Category> paginUsers(@Param("categoryName") String categoryName,@Param("pageNum") Integer pageNum,@Param("pageSize") Integer pageSize);
 
+    //批量删除分类
     int delBatch(List<Integer> ids);
+
+    //获取数据总条数
+    @Select("select count(*) from tb_category where category_name like concat('%',#{categoryName},'%')")
+    Integer getCateCount(@Param("categoryName") String categoryName);
+
+    //获取指定id的分类下绑定的博客数量
+    @Select("select count(*) from tb_blog where tb_blog.category_id=#{id}")
+    Integer getCateBlog(@Param("id") Integer id);
+
+    //获取前十热门分类
+    @Select("select category_name,category_id,count(*) as count from tb_blog,tb_category where tb_blog.category_id=tb_category.id " +
+            "GROUP BY tb_blog.category_id ORDER BY count(*) desc LIMIT 10 ")
+    List<HashMap> getHotCate();
 }
